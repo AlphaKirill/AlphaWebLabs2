87732e93a5b5d3a935b6f78ebd959a2c769395c3
Белкин Кирилл <kirya-belkin@list.ru>
2023-10-06 17:42:43 +0700

    Книги

--- app.py
+++ app.py
@@ -203,6 +203,18 @@ def example():
         {'name':'ФейхуА', 'price': 321},
         {'name':'Апельсины', 'price': 123}
     ]
+    book = [
+        {'autor': 'Роберт Т. Кийосаки', 'genre': 'Предпринимательство, Инвестиции', 'name':'Богатый папа, Бедный папа', 'page': 126},
+        {'autor': 'Алекс Лесли', 'genre': 'Общая психология, Соблазнение', 'name':'Жизнь без трусов', 'page': 220},
+        {'autor': 'Лев Николаевич Толстой', 'genre': 'Роман', 'name':'Война и мир', 'page': 1117},
+        {'autor': 'Фёдор Достоевский', 'genre': 'Роман', 'name':'Преступление и наказание', 'page': 356},
+        {'autor': 'Михаил Шолохов', 'genre': 'Рассказ', 'name':'Судьба человека', 'page': 101},
+        {'autor': 'Дж. Франзен', 'genre': 'Роман', 'name':'Перекрёстки', 'page': 134},
+        {'autor': 'Анна Старобинец', 'genre': 'Роман', 'name':'Лисьи броды', 'page': 87},
+        {'autor': 'Фредрик Бакман', 'genre': 'Роман', 'name':'Медвежий угол', 'page': 111},
+        {'autor': 'Мария Герус', 'genre': 'Фэнтези-роман', 'name':'Крылья', 'page': 176},
+        {'autor': 'Джонатан Свифт', 'genre': 'Роман', 'name':'Путешествия Гулливера', 'page': 202}
+    ]
     return render_template('example.html', 
                            name=name, lab_number=lab_number, rezylt_1=rezylt_1, rezylt_2=rezylt_2,rezylt_3=rezylt_3,
-                            group=group, kurs=kurs, fruits=fruits)
+                            group=group, kurs=kurs, fruits=fruits, book=book)
