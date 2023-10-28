f5c8ee1f867d0c930546acb0282d995b1327b4cd
Белкин Кирилл <kirya-belkin@list.ru>
2023-10-28 18:33:48 +0700

    Валидация

--- lab3.py
+++ lab3.py
@@ -9,7 +9,15 @@ def lab_3():
 
 @lab3.route('/lab3/form1')
 def form_1():
+    errors = {}
+    errors1 = {}
+    user = request.args.get('user')
+    age = request.args.get('age')
+    if user == '' or age == '':
+        errors ['user'] = 'Заполни поле!'
+        errors1 ['age'] = 'Заполни поле!'
+        
     user = request.args.get('user')
     age = request.args.get('age')
     sex = request.args.get('sex')
-    return render_template('form1.html', user=user, age=age, sex=sex)
+    return render_template('form1.html', user=user, age=age, sex=sex, errors=errors, errors1=errors1)
