d598f3e4368c7e1c44218841a5b86691f31c1c52
AlphaKirill <kirya-belkin@list.ru>
2023-12-16 05:05:14 +0700

    Возрат оплаты

--- lab7.py
+++ lab7.py
@@ -1,6 +1,7 @@
-from flask import redirect, render_template, request, Blueprint, abort
+from flask import Blueprint, render_template, request, abort
 
-lab7 = Blueprint('lab7',__name__)
+
+lab7 = Blueprint('lab7', __name__)
 
 
 @lab7.route('/lab7/')
@@ -8,14 +9,13 @@ def main():
     return render_template('lab7/index.html')
 
 
-@lab7.route('/lab7/drink', methods=['GET', 'POST'])
+@lab7.route('/lab7/drink', methods=['GET'])
 def drink():
     return render_template('lab7/drink.html')
 
 
 @lab7.route('/lab7/api', methods=['GET', 'POST'])
 def api():
-
     data = request.json
 
     if data['method'] == 'get-price':
@@ -23,7 +23,10 @@ def api():
 
     if data['method'] == 'pay':
         return pay(data['params'])
-    
+
+    if data['method'] == 'refund':
+        return refund(data['params'])
+
     abort(400)
 
 
@@ -55,10 +58,16 @@ def pay(params):
     card_num = params['card_num']
     if len(card_num) != 16 or not card_num.isdigit():
         return {"result": None, "error": "Неверный номер карты"}
-    
+
     cvv = params['cvv']
     if len(cvv) != 3 or not cvv.isdigit():
         return {"result": None, "error": "Неверный номер CVV/CVC"}
-    
+
     price = calculate_price(params)
     return {"result": f'С карты {card_num} списано {price} руб', "error": None}
+
+
+def refund(params):
+    card_num = params['card_num']
+    price = calculate_price(params)
+    return {"result": f'На карту {card_num} возвращено {price} руб'}
