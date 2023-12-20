dbcc36e0586ed57ec4439b9b6bdb690613c2af0b
AlphaKirill <kirya-belkin@list.ru>
2023-12-21 00:13:47 +0700

    Обработчик ошибки 404

--- lab9.py
+++ lab9.py
@@ -6,3 +6,8 @@ lab9 = Blueprint('lab9', __name__)
 @lab9.route('/lab9/')
 def main():
     return render_template('lab9/index.html')
+
+
+@lab9.app_errorhandler(404)
+def not_found(err):
+    return render_template('lab9/error.html')
